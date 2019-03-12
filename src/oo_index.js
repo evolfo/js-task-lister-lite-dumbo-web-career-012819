d090b70748a5f2e197fbcda449497953a1f4c518
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-12 17:50:02 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,39 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+});
+
+
+const ulTag = document.getElementById('tasks');
+const formTag = document.querySelector('form');
+const descriptionTag = document.getElementById('new-task-description');
+//const buttonTag = document.querySelector(descriptionTag.value);
+const divTag = document.getElementById('list');
+
+let buttonTag = "";
+
+
+formTag.name = "description";
+
+formTag.addEventListener('submit', function(event) {
+  event.preventDefault();
+  ulTag.innerHTML += `
+    <li>${descriptionTag.value}
+      <button data-description="${descriptionTag.value}">X</button>
+    </li>
+  `;
+  // let buttonTag = document.querySelector(`[data-description = "${descriptionTag.value}"]`);
+});
+
+// if (buttonTag !== "") {
+//   buttonTag.addEventListener("click", function(event) {
+//     console.log('hii');
+//     debugger
+//   });
+// }
+
+divTag.addEventListener('click', function(event) {
+  if (event.target.innerHTML == "X") {
+    event.target.parentElement.remove();
+  }
 });
